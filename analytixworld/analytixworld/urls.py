a80916c697645d935299d89b3e8951f26de21f37
Pawan Kumar <pawan@acelrtech.com>
2019-04-18 14:56:27 +0530

    Added django Django REST framework support with basic users listing as starting kit

--- analytixworld/analytixworld/urls.py
+++ analytixworld/analytixworld/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .restfw import *
+from .restfw import router, UserSerializer, UserViewSet
 
 urlpatterns = [
     url(r'^', include(router.urls)),
