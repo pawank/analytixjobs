4fa2db2f0015c85476051f569ccd9367c9150541
Pawan Kumar <pawan@acelrtech.com>
2019-04-26 20:19:59 +0530

    add Contact to Company for associated members

--- analytixworld/companies/models.py
+++ analytixworld/companies/models.py
@@ -5,6 +5,27 @@ from users.models import CustomUser
 from taggit.managers import TaggableManager
 
 
+class Company(Core):
+    name = models.CharField(verbose_name="Company name", max_length=500,
+                            null=True, blank=False)
+    registered_name = models.CharField(verbose_name="Registered Company name", max_length=500,
+                                       null=True, blank=True)
+    no_employees = models.IntegerField(verbose_name="No of Employees",
+                                       null=True, blank=True)
+    website = models.URLField(
+        verbose_name="Website", null=True, blank=True)
+    hash_tags = TaggableManager()
+    # More info at: https://www.revsys.com/tidbits/tips-using-djangos-manytomanyfield/
+    #contacts = models.ManyToManyField('Contact', related_name='companies')
+
+    class Meta:
+        ordering = ('-created_on', 'name')
+        #verbose_name_plural = "Company"
+
+    def __str__(self):
+        return self.name or ''
+
+
 class Contact(Core):
     name = models.CharField(verbose_name="Full name", max_length=500,
                             null=True, blank=False)
@@ -17,25 +38,8 @@ class Contact(Core):
     address = models.TextField(verbose_name="Address",
                                null=True, blank=True)
     hash_tags = TaggableManager()
-
-    class Meta:
-        ordering = ('-created_on', 'name')
-        #verbose_name_plural = "Company"
-
-    def __str__(self):
-        return self.name or ''
-
-
-class Company(Core):
-    name = models.CharField(verbose_name="Company name", max_length=500,
-                            null=True, blank=False)
-    registered_name = models.CharField(verbose_name="Registered Company name", max_length=500,
-                                       null=True, blank=True)
-    no_employees = models.IntegerField(verbose_name="No of Employees",
-                                       null=True, blank=True)
-    website = models.URLField(
-        verbose_name="Website", null=True, blank=True)
-    hash_tags = TaggableManager()
+    parent_company = models.ForeignKey(
+        'companies.company', related_name='members', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('-created_on', 'name')
